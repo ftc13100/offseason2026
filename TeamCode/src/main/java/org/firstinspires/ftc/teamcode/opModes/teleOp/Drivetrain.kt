998c6024f408d7c4f2bc385a860fa1c4cd653de0
MomohitPatil <mo24.patil@gmail.com>
2026-05-30 11:10:31 -0400

    field centric

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/teleOp/Drivetrain.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/teleOp/Drivetrain.kt
@@ -8,11 +8,13 @@ import dev.nextftc.bindings.button
 import dev.nextftc.core.components.BindingsComponent
 import dev.nextftc.core.components.SubsystemComponent
 import dev.nextftc.core.commands.CommandManager
+import dev.nextftc.core.units.rad
 import dev.nextftc.extensions.pedro.PedroComponent
 import dev.nextftc.extensions.pedro.PedroComponent.Companion.follower
 import dev.nextftc.ftc.Gamepads
 import dev.nextftc.ftc.NextFTCOpMode
 import dev.nextftc.ftc.components.BulkReadComponent
+import dev.nextftc.hardware.driving.FieldCentric
 import dev.nextftc.hardware.driving.MecanumDriverControlled
 import dev.nextftc.hardware.impl.MotorEx
 import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit
@@ -98,7 +100,7 @@ class Drivetrain : NextFTCOpMode() {
             -Gamepads.gamepad1.leftStickY,
             Gamepads.gamepad1.leftStickX,
             Gamepads.gamepad1.rightStickX,
-//            mode = FieldCentric { follower.pose.heading.rad }
+            mode = FieldCentric { follower.pose.heading.rad }
         )
         driverControlled.scalar = 1.0
 
